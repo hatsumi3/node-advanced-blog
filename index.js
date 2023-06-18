308ca7d2ffab139aa98a71bb331b5c9b27dfb794
hatsumi3 <al.usamiku39@gmail.com>
2023-06-18 21:32:00 +0900

    feat: add mongo db process.

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
+
+// Mongo DB
+const mongoose = require("mongoose");
+const mongodb = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:${process.env.MONGO_PORT}`;
+
+mongoose
+  .connect(mongodb)
+  .then(() => {
+    console.log("mongo connected");
+  })
+  .catch((error) => {
+    console.error("Failure: Unconnected.");
+    console.error(error);
+  });
+
+const Schema = mongoose.Schema;
+const BlogSchema = new Schema({
+  title: String,
+  summary: String,
+  image: String,
+  textBody: String,
+});
+const BlogModel = mongoose.model("Blog", BlogSchema);
+
+// router
 
 app.get("/", (req, res) => {
   res.send("hello.");
@@ -13,7 +39,20 @@ app.get("/blog/create", (req, res) => {
 
 app.post("/blog/create", (req, res) => {
   console.log(req.body);
-  res.send("saved.");
+
+  //NOTE:
+  // mongooseのversion upによりcallbackでの書き方に変更.
+  // https://stackoverflow.com/questions/75701761/mongooseerror-model-create-no-longer-accepts-a-callback
+
+  BlogModel.create(req.body)
+    .then((savedBlogData) => {
+      console.log("success.");
+      res.send("saved.");
+    })
+    .catch((err) => {
+      console.log("failed data write.");
+      res.send("faied.");
+    });
 });
 
 app.listen(5000, () => {
