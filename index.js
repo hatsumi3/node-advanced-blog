f113cb19c272a0fac9e0bc8e8ec85ea9f04d293f
hatsumi3 <al.usamiku39@gmail.com>
2023-06-18 20:47:12 +0900

    feat: add get/post method.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("hello.");
+});
+
+app.get("/blog/create", (req, res) => {
+  res.sendFile(__dirname + "/views/blogCreate.html");
+});
+
+app.post("/blog/create", (req, res) => {
+  console.log(req.body);
+  res.send("saved.");
 });
 
 app.listen(5000, () => {
